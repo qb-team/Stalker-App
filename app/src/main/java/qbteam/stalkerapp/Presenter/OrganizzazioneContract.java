ed32af1f907bd46eff3fa8e54adfce7a43504d3f
Drago Francesco <francydrake97@gmail.com>
2020-04-24 10:26:45 +0200

    Revert "aggiunto dialog-info"
    This reverts commit 881f734ce64eff00c764829451b6d57662b35d91.

---
+++ app/src/main/java/qbteam/stalkerapp/Presenter/OrganizzazioneContract.java
@@ -0,0 +1,21 @@
+package com.example.stalkerapp.Presenter;
+
+
+
+public interface OrganizzazioneContract {
+
+    interface View {
+
+    }
+
+    //METODO DEL PRESENTER CHE VA A CHIAMARE IL METODO DELL' INTRACTOR DEL MODELLO
+    interface Presenter {
+
+    }
+
+    //METODO DEL MODELLO
+    interface Intractor {
+
+    }
+
+}
